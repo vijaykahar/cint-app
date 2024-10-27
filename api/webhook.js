2015f0f9398879d5196fd9be6c93a45e1ddad505
Vijay Kahar <vijay.kahar@consensus.com>
2024-10-28 04:04:41 +0530

    adsad

--- api/webhook.js
+++ api/webhook.js
@@ -3,21 +3,24 @@
 let webhookEvents = []; // Use a variable for storing events
 
 export default function handler(req, res) {
-    // if (req.method === 'POST') {
-    //     console.log('Webhook received:', req.body);
+    if (req.method === 'POST') {
+        alert("post")
+        console.log('Webhook received:', req.body);
 
-    //     // Store the event in memory
-    //     webhookEvents.push(req.body);
+        // Store the event in memory
+        webhookEvents.push(req.body);
         
-    //     // Optionally limit the number of stored events
-    //     if (webhookEvents.length > 1000) {
-    //         webhookEvents.shift(); // Remove the oldest event if over limit
-    //     }
+        // Optionally limit the number of stored events
+        if (webhookEvents.length > 1000) {
+            webhookEvents.shift(); // Remove the oldest event if over limit
+        }
 
-    //     res.status(200).send('Webhook received');
-    // } else if (req.method === 'GET') {
-    //     res.status(200).json(webhookEvents);
-    // } else {
-    //     res.status(405).json({ message: 'Method not allowed' });
-    // }
+        res.status(200).send('Webhook received');
+    } else if (req.method === 'GET') {
+        alert("get")
+        res.status(200).json(webhookEvents);
+    } else {
+        alert("else")
+        res.status(405).json({ message: 'Method not allowed' });
+    }
 }

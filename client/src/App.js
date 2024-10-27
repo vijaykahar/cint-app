12000325365ad498fbc513c883001c31eeedc4db
Vijay Kahar <vijay.kahar@consensus.com>
2024-10-27 21:19:48 +0530

    third commit

--- client/src/App.js
+++
@@ -1,9 +0,0 @@
-const sendTestWebhook = async () => {
-  try {
-      const response = await axios.post('/api/webhook', { key: 'value' });
-      setResponseMessage(response.data.message);
-  } catch (error) {
-      console.error("Error sending webhook:", error);
-      setResponseMessage('Error sending webhook');
-  }
-};
